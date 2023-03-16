ed0e5eca8e189386a3cff489782ccfe701f93412
Jennifer Yiu <jen.yiu.90@gmail.com>
2023-03-15 21:26:34 -0700

    update about write-up

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -26,40 +26,15 @@ const About = () => {
         />
 
         <p className="text">
-          I am a full-stack developer who's passionate about the future of tech, problem-solving, and improving team dynamics. My curiosity about the
-          world around me drives me to learn and grow - whether it’s memorizing over 750 digits of pi, solving a Rubik’s cube, becoming a certified
-          scuba diver, understanding blockchain, or backpacking alone in Europe and
-          Southeast Asia.
+          As a full-stack developer, I am driven by my passion for the future of technology, creative problem-solving, and fostering positive team dynamics. My insatiable curiosity fuels my personal and professional growth, whether I'm memorizing over 750 digits of pi, solving a Rubik’s cube, scuba diving, understanding blockchain, or backpacking solo through Europe and Southeast Asia.
         </p>
         <p className="text">
-          My first exposure to code was at 6 years old. My brother showed me how
-          to write scripts to play a simple song on the computer. I was
-          enthralled by how a combination of characters and symbols can turn the
-          computer into a piano. In sixth grade, I built my first website using
-          HTML.
+          My love for coding started at an early age when my brother taught me how to write scripts to play a simple song on the computer. Since then, I've honed my skills, building my first website using HTML in sixth grade, and advancing my knowledge of front-end technologies like React, React-Native, JavaScript, jQuery, CSS, and SCSS. I've also gained experience with back-end technologies such as Python, SQL, and Django and remain committed to expanding my expertise.
         </p>
-        {/* <p className="text">
-              As I dive deeper into the world of development and technology, my
-              passion for it grows stronger. It’s challenging, evolving, and
-              it’s the future. The tech world is ever-changing and I want to
-              work in a team to create something amazing together.
-            </p> */}
 
-        <p className="text">
-          My primary focus is in front-end technologies including
-          <span className="techs">
-            {' '}
-            React, React-Native, JavaScript, jQuery, CSS, SCSS, and PHP.{' '}
-          </span>
-          I've also been working a lot with back-end technologies such as{' '}
-          <span className="techs">Python, SQL, Django, and Grails/Groovy</span> and am
-          interested to learn more! I have a growth mindset with a passion to learn and to build the best project in any team I'm in.
-        </p>
         <VideoPlayer link={urllink} />
         <p className="text">
-          If you love discussing new ideas or are in tech, let’s connect! I’d
-          love to chat about new technology, my expertise, your expertise and
-          how we might work together!
+          With a growth mindset, I am always eager to learn and thrive in collaborative environments that push me to be my best. If you're interested in discussing new ideas or are in the tech industry, I'd love to connect and explore potential opportunities to work together.
         </p>
       </section>
     </div>
